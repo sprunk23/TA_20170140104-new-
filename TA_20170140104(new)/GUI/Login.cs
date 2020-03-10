d4212bf2e6bfb3c2d8f921e34b028ab584cd45e1
Zuhad Fathoni <zuhadfat1123@gmail.com>
2020-03-10 22:24:21 +0700

    login selesai

--- TA_20170140104(new)/GUI/Login.cs
+++ TA_20170140104(new)/GUI/Login.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 
 namespace TA_20170140104_new_.GUI
 {
+	/// <summary>
+	/// class <c>Login</c> login halaman awal
+	/// </summary>
+
 	public partial class Login : Form
 	{
 		public Login()
@@ -17,6 +21,15 @@ namespace TA_20170140104_new_.GUI
 			InitializeComponent();
 			password_txt.PasswordChar = '*';
 		}
+
+		/// <summary>
+		/// button login untuk login
+		/// </summary>
+		/// <returns>
+		/// error
+		/// </returns>
+		/// <param name="sender"><c>object sender</c> parameter yang disebut Pengirim yang berisi referensi ke kontrol / objek yang mengangkat peristiwa.</param>
+		/// <param name="e"><c>EventArgs e</c> parameter yang disebut e yang berisi data peristiwa.</param>
 
 		private void button1_Click(object sender, EventArgs e)
 		{
